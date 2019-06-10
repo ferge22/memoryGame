e346b71a165ae343575a806f7c461affd52efdee
ferge22 <rytisspitrys@gmail.com>
2019-06-10 17:17:50 +0300

    implementing logic part in js file

--- scripts.js
+++ scripts.js
@@ -1,10 +1,74 @@
-let allCards = document.querySelectorAll('.card');
+"use strict";
 
+let allCards = document.querySelectorAll(".card");
+let resetBtn = document.getElementById("reset");
 
+let isFlipped = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.add('flip');
+  //  not a match lock board
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+  // first card clicked sets to true, this targets to clicked card
+  if (!isFlipped) {
+    isFlipped = true;
+    firstCard = this;
+    // stop function
+    return;
+  }
+  // second card clicked sets to false, this targets to clicked card
+  secondCard = this;
+  lockBoard = true;
+
+  checkMatchedCards();
 }
 
+function checkMatchedCards() {
+  //  if names matches
+  let isMatch = firstCard.dataset.match === secondCard.dataset.match;
+  // if isMatch true "stopCards()" else false do "throwBack()"
+  isMatch ? stopCards() : throwBack();
+}
 
-allCards.forEach(card => card.addEventListener('click', flipCard));
+function stopCards() {
+  //  unable to click on matched cards
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function throwBack() {
+  // unmatched cards loses flip class
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  isFlipped = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+// loop through all cards, on click execute flipCard function
+allCards.forEach(card => card.addEventListener("click", flipCard));
+resetBtn.addEventListener('click', ()=> {
+    location.reload();
+});
+
+// Iffe invokes imeadetly shuffling cards to random positions
+(function shuffle() {
+  allCards.forEach(card => {
+    // Number between 0 to 15;
+    let randomPosition = Math.ceil(Math.random() * 16);
+    card.style.order = randomPosition;
+  });
+})();
